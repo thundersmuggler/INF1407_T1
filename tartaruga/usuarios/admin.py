145d0546d3725280a3cfa87f56c5d92510b8db58
Felipe Sette <frss777@gmail.com>
2024-10-16 23:36:55 -0300

    criação de view

--- tartaruga/usuarios/admin.py
+++ tartaruga/usuarios/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+
+from usuarios.models import Pessoa
+
+admin.site.register(Pessoa)
--- tartaruga/usuarios/views.py
+++ tartaruga/usuarios/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from usuarios.models import Pessoa
+from django.views.generic.base import View
+
+class UsuarioListView(View):
+    def get(self, request, *args, **kwargs):
+        pessoas = Pessoa.objects.all()
+        contexto = { 'pessoas': pessoas, }
+        return render(
+            request,
+            'contatos/listaContatos.html',
+            contexto
+        )
